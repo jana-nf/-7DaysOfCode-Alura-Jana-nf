9bb2c06dcae17988998424730bf8e4f092f50f24
Janaina Nascimento <janainanascimentofeitoza@gmail.com>
2025-02-25 10:59:07 -0300

    Código Dia 1/7

---
+++ LogicaOperacoesBooleanas.js
@@ -0,0 +1,30 @@
+// Reescrever o código abaixo de maneira que ele imprima as informações de maneira correta, sem erros
+// Utilize os operadores lógicos booleanos para isso.
+
+let numeroUm = 2;
+let stringUm = "2";
+let numeroTrinta = 50;
+let stringTrinta = "50";
+let numeroDez = 90;
+let stringDez = "90";
+
+
+if (numeroDois == stringDois) {
+    console.log("As variáveis numeroDois e stringDois tem o mesmo valor, mas tipos diferentes");
+} else {
+    console.log("As variáveis numeroDois e stringDois não tem o mesmo valor");
+}
+
+
+if (numeroCinquenta === stringCinquenta) {
+    console.log("As variáveis numeroCinquenta e stringCinquenta tem o mesmo valor e mesmo tipo");
+} else {
+    console.log("As variáveis numeroCinquenta e stringCinquenta não tem o mesmo tipo");
+}
+
+
+if (numeroNoventa == stringNoventa) {
+    console.log("As variáveis numeroNoventa e stringNoventa tem o mesmo valor, mas tipos diferentes");
+} else {
+    console.log("As variáveis numeroNoventa e stringNoventa não tem o mesmo valor");
+}
